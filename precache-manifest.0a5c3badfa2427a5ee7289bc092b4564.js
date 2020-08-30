6097d30733f269343f3fdd16c8fa1b3ebd4f6020
kristina-gerko <k.gerko007@gmail.com>
2020-08-30 14:08:09 +0300

    Updates

---
+++ precache-manifest.0a5c3badfa2427a5ee7289bc092b4564.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c879a3c91cc2497c1576a79557900b",
+    "url": "/aviasales-task/index.html"
+  },
+  {
+    "revision": "77bf2c1bf044bf89c645",
+    "url": "/aviasales-task/static/css/main.5104d039.chunk.css"
+  },
+  {
+    "revision": "edb40db658a579dd21c3",
+    "url": "/aviasales-task/static/js/2.3bf8d297.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/aviasales-task/static/js/2.3bf8d297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77bf2c1bf044bf89c645",
+    "url": "/aviasales-task/static/js/main.a8cbd3d0.chunk.js"
+  },
+  {
+    "revision": "c66cc15ba4c3f1109da6",
+    "url": "/aviasales-task/static/js/runtime-main.c47fef91.js"
+  },
+  {
+    "revision": "6a639fae13a42517aada1f1f81cc6356",
+    "url": "/aviasales-task/static/media/cross-icon.6a639fae.svg"
+  },
+  {
+    "revision": "1cbc88dc1b3ff746bfa58e181d7f6b7d",
+    "url": "/aviasales-task/static/media/logo.1cbc88dc.svg"
+  }
+]);
